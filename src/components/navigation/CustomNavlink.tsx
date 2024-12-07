3de3cc8f8f1f412dc68cd9bab78ff3796e9b84ce
Kelvin Ajayi <68742854+scryptwiz@users.noreply.github.com>
2024-12-07 16:06:09 +0100

    Update CustomNavlink.tsx

--- src/components/navigation/CustomNavlink.tsx
+++ src/components/navigation/CustomNavlink.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { NavLink, NavLinkProps, useLocation } from 'react-router-dom'
 import { Link as LinkScroll } from 'react-scroll';
 import { NavLinkProps, useLocation } from 'react-router-dom'
 
@@ -7,18 +6,15 @@ interface CustomNavLinkProps extends NavLinkProps {
 	to: string;
 	children: React.ReactNode;
 	customClass?: string;
-	onClick?: () => void;
 }
 
 
-const CustomNavlink: React.FC<CustomNavLinkProps> = ({ to, children, customClass, onClick }) => {
+const CustomNavlink: React.FC<CustomNavLinkProps> = ({ to, children, customClass }) => {
 	const location = useLocation();
-
-	// Check if the current hash matches the `to` value
 	const isActive = location.hash === to;
 
 	return (
-		<LinkScroll to={to} offset={-100} spy smooth onClick={onClick} className={`relative font-medium transition-all duration-700 ease-in-out cursor-pointer hover:text-active ${isActive ? "text-active scale-110 font-bold" : "text-inherit"} ${customClass}`}>
+		<LinkScroll to={to} offset={-100} spy smooth className={`relative font-medium transition-all duration-700 ease-in-out cursor-pointer hover:text-active ${isActive ? "text-active scale-110 font-bold" : "text-inherit"} ${customClass}`}>
 			<span>
 				{isActive && <p className='inline mr-1'>&lt;</p>}
 				{children}
