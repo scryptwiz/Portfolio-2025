89f47f1470e9a7f0e13a423003474d4ca3fe2d24
Kelvin Ajayi <68742854+scryptwiz@users.noreply.github.com>
2024-12-09 19:32:53 +0100

    Update hero.tsx

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
 					</h1>
 				</Typist>
 				<FadeInAnimation>
-					<p className="max-w-lg mx-auto text-center font-medium mt-3 tracking-wide text-sm">I design 3D visuals and UIs for web apps and websites, and specialize in full-stack development.</p>
+					<p className="max-w-lg mx-auto text-center font-medium mt-3 tracking-wide text-sm">I design 3D visuals and UIs for apps and websites, and specialize in full-stack development.</p>
 					<img src={HeroImage2} alt="Kelvin's hero image" className="md:max-w-lg w-full h-auto mt-8" />
 				</FadeInAnimation>
 			</div>
